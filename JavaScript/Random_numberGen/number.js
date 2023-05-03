a2f85bc05a7392c6ac8c65b90f34ab6094604c5c
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-05-03 12:47:00 +0530

    Random number  Generator

---
+++ JavaScript/Random_numberGen/number.js
@@ -0,0 +1,6 @@
+//random number on integral part
+const num1 = Math.floor((Math.random()*100) + 1);
+console.log("This is a rRndom number(1-100): "+ num1);
+
+const num2 = Math.floor((Math.random()*1000) + 1);
+console.log("This is a rRndom number(1-1000): "+ num2);
