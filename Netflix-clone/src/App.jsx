8ad3d80d474dd93d37e5c9bc2223ff8d6acf58e1
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-05-31 01:58:00 +0530

    Minor update routes added- App.jsx to render video at the Player

--- Netflix-clone/src/App.jsx
+++ Netflix-clone/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Netflix from './pages/Netflix'
 import Login from './pages/Login'
