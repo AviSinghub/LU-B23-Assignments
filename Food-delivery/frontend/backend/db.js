c2c41534ddd2b0e01e83eca769c12bd48ade8af1
Avinash kumar Singh <78693452+AviSinghub@users.noreply.github.com>
2023-09-23 20:19:08 +0530

    Update db.js with no reference

--- Food-delivery/frontend/backend/db.js
+++ Food-delivery/frontend/backend/db.js
@@ -1,10 +1,1 @@
-const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://Biryani:MernBiS@foodhunter.imckhq0.mongodb.net/?retryWrites=true&w=majority'
 
-const mongoDB =()=>{
-    mongoose.connect(mongoURI,() =>{
-        console.log("connected now with mongoDB");
-    } );
-}
-
-module.exports = mongoDB
